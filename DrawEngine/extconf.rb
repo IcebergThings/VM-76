766050d2384d9b789e7c14c25018293dae159581
Frog Chen <satgo1546@yeah.net>
2016-07-11 19:34:44 +0800

    少许代码格式调整
    其实只改了DrawEngine/extconf.rb。

--- DrawEngine/extconf.rb
+++ DrawEngine/extconf.rb
@@ -1,12 +1,10 @@
-#extconf.rb
-require "mkmf"
+# extconf.rb
+require 'mkmf'
 
-extension_name = 'VMDE'
+extension_name = "VMDE"
 
 dirs = ["/usr/local/lib"]
-
-$libs += "-lGLEW -lGLU -lm -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXi -lXcursor -lXrender -lGL -lm -lpthread -ldl -ldrm -lXdamage -lXfixes -lX11-xcb -lxcb-glx -lxcb-dri2 -lXxf86vm -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp "
+$libs += "-lGLEW -lGLU -lm -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXi -lXcursor -lXrender -lGL -lm -lpthread -ldl -ldrm -lXdamage -lXfixes -lX11-xcb -lxcb-glx -lxcb-dri2 -lXxf86vm -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp"
 
 dir_config(extension_name)
-
 create_makefile(extension_name)
