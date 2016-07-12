52dedde8c3988361bbb2d14a843e5db280fa9a54
Frog Chen <satgo1546@yeah.net>
2016-07-12 21:59:25 +0800

    time(NULL)应当是正确用法

--- VMDE/main.cpp
+++ VMDE/main.cpp
@@ -4,18 +4,18 @@ void glfw_error_callback(int error, const char* description) {
 	printf(DBG_HEAD "[glfw_error_callback]: GL Error!\n\t%d: %s\n", error, description);
 }
 
-long double sysTime = time(0);
+long double sysTime = time(NULL);
 long double sysTimeMS = sysTime * 1000;
 int temp_fps_c = 0;
 
 void main_draw_loop() {
 	VMDE->framecount++;
 	temp_fps_c++;
-	if (time(0) >= sysTime + 1) {
+	if (time(NULL) >= sysTime + 1) {
 		VMDE->fps = temp_fps_c;
 		temp_fps_c = 0;
 
-		sysTime = time(0);
+		sysTime = time(NULL);
 		sysTimeMS = sysTime * 1000;
 	}
 	if (!VMDE->States->freeze) {
