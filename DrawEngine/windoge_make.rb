d3a1fe7ffe0c90bc79eb25b97592e9b148b00b64
Frog Chen <satgo1546@yeah.net>
2016-07-12 21:19:32 +0800

    Update DrawEngine/windoge_make.rb to be more intelligent
    I made a module wrapper in that script. This is good because I can
    insert RPG Maker styled comment blocks then.
    
    The script can now output a batch file if it's not been generated
    before and after that, you can double-click on the batch file directly
    to compile the Ruby extension.

--- DrawEngine/windoge_make.rb
+++ DrawEngine/windoge_make.rb
@@ -1,45 +1,78 @@
 # 如果加了这个 → !/usr/bin/cat
 # 就不能用了。ruby: no Ruby script found in input (LoadError)
-if ARGV.size != 4
-	puts "= Example ="
-	puts '> ruby windoge_make.rb "C:\\Program Files\\Ruby" 2.3.0 D:\\glfw-3.2.bin.WIN32 D:\\glew-1.13.0'
-	exit
+#==============================================================================
+# ■ windoge_make.rb
+#------------------------------------------------------------------------------
+#   君住Linux中，我住Windows里。日日思君不见君，共用编译器。
+#   警告几时休，错误何时已？只愿君心似我心，定不负移植意。
+#   ——Frog Chen《卜算子（伪）·编译》
+#==============================================================================
+
+module WindogeMake
+	BATCH_FILENAME = "windoge_make.bat"
+	#--------------------------------------------------------------------------
+	# ● 主程序
+	#--------------------------------------------------------------------------
+	def self.main(argv = ARGV)
+		if argv.size != 4
+			puts "= Example ="
+			puts "> ruby windoge_make.rb C:\\Ruby23 2.3.0 D:\\glfw-3.2.bin.WIN32 D:\\glew-1.13.0"
+			puts "> rem 作为开发者，你不应该在此类目录的名称中包含空格。"
+			exit
+		end
+		ruby_path, ruby_version, glfw_path, glew_path = argv
+		sources = Dir.glob("*.cpp")
+		objects = []
+		dll_name = "VMDE.dll" # .dll: required to build DLL (maybe?)
+		so_name = "VMDE.so" # .so: required for Ruby to work
+		# some ugly hacks for Windoge
+		# 如果不这么搞就会LoadError
+		sources.each do |source_name|
+			name = File.basename(source_name, ".cpp")
+			command = "g++ -c #{source_name} -o #{name}.o -O3 -Wall -Wextra"
+			command << " -I#{ruby_path}\\include\\ruby-#{ruby_version}"
+			command << " -I#{ruby_path}\\include\\ruby-#{ruby_version}\\i386-mingw32"
+			command << " -D__MINGW_USE_VC2005_COMPAT -D_FILE_OFFSET_BITS=64"
+			command << " -fno-omit-frame-pointer"
+			# GLFW & GLEW
+			command << " -DGLFW_DLL -DGLEW_STATIC"
+			command << " -I#{glfw_path}\\include"
+			command << " -I#{glew_path}\\include"
+			make command
+			objects << "#{name}.o"
+		end
+		command = "gcc #{objects.join(" ")} -s -shared -o #{dll_name} -lstdc++"
+		command << " -L#{ruby_path}\\lib"
+		command << " -Wl,--enable-auto-image-base,--enable-auto-import"
+		command << " -lmsvcrt-ruby230 -lshell32 -lws2_32 -liphlpapi -limagehlp -lshlwapi"
+		command << " -L#{glfw_path}\\lib-mingw-w64"
+		command << " -L#{glew_path}\\lib\\Release\\Win32"
+		command << " -Wl,-subsystem,windows -lglfw3dll -lglew32s -lopengl32"
+		make command
+		# For tools like exeScope, the file must have correct suffix.
+		# However for Ruby, the DLL must have corrupted suffix.
+		# 而这就是PY交易。
+		File.rename(dll_name, so_name)
+	end
+	#--------------------------------------------------------------------------
+	# ● 模拟Make的一步行动
+	#--------------------------------------------------------------------------
+	def self.make(command)
+		puts command
+		exit unless system command
+	end
+	#--------------------------------------------------------------------------
+	# ● 输出能够快捷地执行本脚本的批处理文件
+	#--------------------------------------------------------------------------
+	def self.output_batch(argv = ARGV)
+		File.open(BATCH_FILENAME, "w") do |f|
+			f.puts "ruby windoge_make.rb #{argv.join(" ")}"
+			f.puts "@pause"
+		end
+	end
 end
-# 模拟Make的行动
-def make(command)
-	puts command
-	exit unless system command
+
+if $0 == __FILE__
+	WindogeMake.main
+	WindogeMake.output_batch unless FileTest.exist?(WindogeMake::BATCH_FILENAME)
 end
-ruby_path, ruby_version, glfw_path, glew_path = ARGV
-sources = Dir.glob("*.cpp")
-objects = []
-dll_name = "VMDE.dll" # .dll: required to build DLL (maybe?)
-so_name = "VMDE.so" # .so: required for Ruby to work
-# some ugly hacks for Windoge
-# 如果不这么搞就会LoadError
-sources.each do |source_name|
-	name = File.basename(source_name, ".cpp")
-	command = "g++ -c #{source_name} -o #{name}.o -O3 -Wall -Wextra"
-	command << " -I#{ruby_path}\\include\\ruby-#{ruby_version}"
-	command << " -I#{ruby_path}\\include\\ruby-#{ruby_version}\\i386-mingw32"
-	command << " -D__MINGW_USE_VC2005_COMPAT -D_FILE_OFFSET_BITS=64"
-	command << " -fno-omit-frame-pointer"
-	# GLFW & GLEW
-	command << " -DGLFW_DLL -DGLEW_STATIC"
-	command << " -I#{glfw_path}\\include"
-	command << " -I#{glew_path}\\include"
-	make command
-	objects << "#{name}.o"
-end
-command = "gcc #{objects.join(" ")} -s -shared -o #{dll_name} -lstdc++"
-command << " -L#{ruby_path}\\lib"
-command << " -Wl,--enable-auto-image-base,--enable-auto-import"
-command << " -lmsvcrt-ruby230 -lshell32 -lws2_32 -liphlpapi -limagehlp -lshlwapi"
-command << " -L#{glfw_path}\\lib-mingw-w64"
-command << " -L#{glew_path}\\lib\\Release\\Win32"
-command << " -Wl,-subsystem,windows -lglfw3dll -lglew32s -lopengl32"
-make command
-# For tools like exeScope, the file must have correct suffix.
-# However for Ruby, the DLL must have corrupted suffix.
-# 而这就是PY交易。
-File.rename(dll_name, so_name)
