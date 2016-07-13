a163dc4dfbc2ebd1e62dc7492e704f0088f3d911
bobcao3 <bobcaocheng@163.com>
2016-07-13 23:47:01 +0800

    干空格改tab

--- VMDE/init.cpp
+++ VMDE/init.cpp
@@ -24,55 +24,55 @@ const GLchar* temp_fragmentShaderSource = "#version 330 core\n"
     "}\n\0";
 
 int init_shaders() {
-  const GLchar* basic_2D_vsh_src = temp_vertexShaderSource;
+	const GLchar* basic_2D_vsh_src = temp_vertexShaderSource;
 
-  basic_2D_vsh = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(basic_2D_vsh, 1, &basic_2D_vsh_src, NULL);
-  glCompileShader(basic_2D_vsh);
-  GLint success;
-  GLchar infoLog[512];
-  glGetShaderiv(basic_2D_vsh, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(basic_2D_vsh, 512, NULL, infoLog);
-    log("VSH compilation failed:\n%s", infoLog);
-    return 301;
-  }
+	basic_2D_vsh = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(basic_2D_vsh, 1, &basic_2D_vsh_src, NULL);
+	glCompileShader(basic_2D_vsh);
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(basic_2D_vsh, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(basic_2D_vsh, 512, NULL, infoLog);
+		log("VSH compilation failed:\n%s", infoLog);
+		return 301;
+	}
 
-  // Fragment shader
-  const GLchar* basic_2D_fsh_src = temp_fragmentShaderSource;
+	// Fragment shader
+	const GLchar* basic_2D_fsh_src = temp_fragmentShaderSource;
 
-  basic_2D_fsh = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(basic_2D_fsh, 1, &basic_2D_fsh_src, NULL);
-  glCompileShader(basic_2D_fsh);
-  glGetShaderiv(basic_2D_fsh, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(basic_2D_fsh, 512, NULL, infoLog);
-    log("FSH compilation failed:\n%s", infoLog);
-    return 302;
-  }
+	basic_2D_fsh = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(basic_2D_fsh, 1, &basic_2D_fsh_src, NULL);
+	glCompileShader(basic_2D_fsh);
+	glGetShaderiv(basic_2D_fsh, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(basic_2D_fsh, 512, NULL, infoLog);
+		log("FSH compilation failed:\n%s", infoLog);
+		return 302;
+	}
 
-  // Link shaders
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, basic_2D_vsh);
-  glAttachShader(shaderProgram, basic_2D_fsh);
-  glLinkProgram(shaderProgram);
-  // Check for linking errors
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    log("shaders linking failed:\n%s", infoLog);
-    return 303;
-  }
-  glDeleteShader(basic_2D_vsh);
-  glDeleteShader(basic_2D_fsh);
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, basic_2D_vsh);
+	glAttachShader(shaderProgram, basic_2D_fsh);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		log("shaders linking failed:\n%s", infoLog);
+		return 303;
+	}
+	glDeleteShader(basic_2D_vsh);
+	glDeleteShader(basic_2D_fsh);
 
-  return 0;
+	return 0;
 }
 
 void setup_viewport() {
-  int width, height;
-  glfwGetFramebufferSize(window, &width, &height);
-  glViewport(0, 0, width, height);
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 }
 
 int init_engine(int w, int h) {
@@ -108,10 +108,10 @@ int init_engine(int w, int h) {
 	// 初始化GLEW
 	if (glewInit() != GLEW_OK) rb_raise(rb_eRuntimeError, "glewInit() (GLEW Initialization) failed.");
 
-  setup_viewport();
+	setup_viewport();
 
-  glGenBuffers(1, &VBO[0]);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glGenBuffers(1, &VBO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 
 	// 初始化着色器「OpenGL 3.2没有固定管线了，着色器是被钦定的」
 	int rc = init_shaders();
