63e070cbe9ff7bb15348dc6c9a83748640bc16b4
Frog Chen <satgo1546@yeah.net>
2016-07-27 14:05:53 +0800

    Linux平台测试正常
    我对extconf.rb和test.rb开头都没有#!/usr/bin/env ruby而感到十分惊讶。

--- VMDE/extconf.rb
+++ VMDE/extconf.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 #==============================================================================
 # ■ extconf.rb
 #------------------------------------------------------------------------------
@@ -13,7 +14,7 @@ dirs = ["/usr/local/lib"]
 $libs += %w(
 	-lGLEW -lGLU -lglfw -lrt -lportaudio -lasound -lXrandr -lXinerama -lXi -lXcursor -lXrender
 	-lGL -lm -ldl -ldrm -lXdamage -lXfixes -lX11-xcb -lxcb-glx -lxcb-dri2
-	-lXxf86vm -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp
+	-lXxf86vm -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp -lvorbisfile
 ).join(" ")
 
 dir_config(extension_name)
