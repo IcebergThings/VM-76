a67d8c3890ba8769c0dd492f4ca377c51527d1ca
Cheng Cao <bobcaocheng@163.com>
2016-10-20 12:45:37 +0800

    改为NEAREST
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- VMDE/resources.cpp
+++ VMDE/resources.cpp
@@ -16,8 +16,8 @@ namespace Res {
 		glBindTexture(GL_TEXTURE_2D, this->texture);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 		char* uniform_name = new char[16];
 		sprintf(uniform_name, "colortex%d", index);
