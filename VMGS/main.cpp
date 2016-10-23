c1c75a5efbe70999ba542c6e39c3cf5c5a7f2504
Cheng Cao <bobcaocheng@163.com>
2016-10-23 14:46:20 +0800

    ...

--- VMGS/main.cpp
+++ VMGS/main.cpp
@@ -7,10 +7,11 @@ namespace VM76 {
 		for (;;) {
 			::main_draw_start();
 
-			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			glEnable(GL_BLEND);
+			glEnable(GL_DEPTH_TEST);
 			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 			// Activate Shader
@@ -20,28 +21,42 @@ namespace VM76 {
 			GLuint model_location = glGetUniformLocation(main_shader->shaderProgram, "brightness");
 			glUniform1f(model_location, VMDE->state.brightness);
 
-			matrix2D();
-
 			// Setup textures
 			for (int index = 0; index < 16; index++) if (Res::tex_unit[index]) {
 				char* uniform_name = new char[16];
 				sprintf(uniform_name, "colortex%d", index);
 				glActiveTexture(GL_TEXTURE0 + index);
 				glBindTexture(GL_TEXTURE_2D, Res::tex_unit[index]->texture);
-				glUniform1i(glGetUniformLocation(main_shader->shaderProgram, (GLchar*) uniform_name), index);
+				glUniform1i(glGetUniformLocation(main_shader->shaderProgram, (GLchar*) uniform_name), Res::tex_unit[index]->texture);
 				xefree(uniform_name);
 			}
+
+			matrix2D();
+			GDrawable::draw(test_obj, projection, view);
 
 			::main_draw_end();
 		}
 	}
+
+	static GLfloat vtx[] = {
+		0.0, 0.0, 0.0,   1.0, 1.0, 1.0, 1.0,  0.0, 0.0,
+		0.0, 16.0, 0.0,  1.0, 1.0, 1.0, 1.0,  0.0, 1.0,
+		16.0, 16.0, 0.0, 1.0, 1.0, 1.0, 1.0,  1.0, 1.0,
+		16.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,  1.0, 0.0
+	};
+	static GLuint itx[] = {0, 1, 3,  1, 2, 3};
 
 	void start_game() {
 		::init_engine(800, 600);
 
 		new Res::Texture((char*)"../Media/terrain.png", 0);
 		test_obj = GDrawable::create();
-//		GDrawable::fbind(test_obj);
+		test_obj->vtx_c = sizeof(vtx) / sizeof(GLfloat);
+		test_obj->ind_c = sizeof(itx) / sizeof(GLuint);
+		test_obj->vertices = vtx;
+		test_obj->indices = itx;
+		test_obj->tri_mesh_count = sizeof(itx) / sizeof(GLuint) / 3;
+		GDrawable::fbind(test_obj);
 
 		loop();
 	}
