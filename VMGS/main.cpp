3198a145f3825343c3bb5d150b417e23a21d5a25
Cheng Cao <bobcaocheng@163.com>
2016-10-23 20:29:09 +0800

    显示出了4种方块

--- VMGS/main.cpp
+++ VMGS/main.cpp
@@ -1,7 +1,45 @@
 #include <VMDE/VMDE.hpp>
 
 namespace VM76 {
-	GDrawable::GDrawable* test_obj = NULL;
+	class Tile {
+	public:
+		GDrawable::GDrawable* obj = NULL;
+
+	private:
+		GLfloat *vtx = NULL;
+		GLuint *itx = NULL;
+
+	public:
+		Tile(int tid) {
+			int x = tid % 16;
+			int y = tid / 16;
+			float T = 1.0f / 16.0f;
+			float S = 0.0f;
+			float xs = x * T;
+			float ys = y * T;
+			vtx = new GLfloat[8 * 9] {
+				0.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,  xs+S, ys+S,
+				0.0, 1.0, 0.0,  1.0, 1.0, 1.0, 1.0,  xs+S, ys+T,
+				1.0, 1.0, 0.0,  1.0, 1.0, 1.0, 1.0,  xs+T, ys+T,
+				1.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,  xs+T, ys+S,
+				0.0, 0.0, 1.0,  1.0, 1.0, 1.0, 1.0,  xs+S, ys+T,
+				0.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0,  xs+T, ys+T,
+				1.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0,  xs+T, ys+S,
+				1.0, 0.0, 1.0,  1.0, 1.0, 1.0, 1.0,  xs+S, ys+S,
+			};
+			itx = new GLuint[3 * 6 * 2] {0,1,2, 1,2,3, 4,5,7, 5,6,7, 0,4,5, 0,5,1, 2,6,7, 2,3,7, 6,1,5, 6,1,2, 0,3,4, 3,4,7};
+			obj = GDrawable::create();
+			obj->vtx_c = 8 * 9;
+			obj->ind_c = 6 * 2 * 3;
+			obj->vertices = vtx;
+			obj->indices = itx;
+			obj->tri_mesh_count = 6 * 2;
+			GDrawable::fbind(obj);
+		}
+	};
+
+	// 暂时初始化前16个方块
+	Tile* t[16];
 
 	void loop() {
 		for (;;) {
@@ -40,8 +78,8 @@ namespace VM76 {
 			//test_obj->model = glm::rotate(glm::mat4(1.0f), 0.01f * VMDE->frame_count, glm::vec3(0.0f, 1.0f, 0.0f));
 			for (int x = -2; x < 3; x++) {
 				for (int y = -2; y < 3; y++) if (x != 0 && y != 0) {
-					test_obj->model = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f + float(x), 0.0f, -0.5f + float(y)));
-					GDrawable::draw(test_obj, projection, view);
+					t[x + 2]->obj->model = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f + float(x), 0.0f, -0.5f + float(y)));
+					GDrawable::draw(t[x + 2]->obj, projection, view);
 				}
 			}
 
@@ -50,31 +88,15 @@ namespace VM76 {
 	}
 
 	const GLfloat tile_size = 1.0f / 16.0f;
-	#define T tile_size
-	#define S 0.0f
-	static GLfloat vtx[] = {
-		0.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,  S, S,
-		0.0, 1.0, 0.0,  1.0, 1.0, 1.0, 1.0,  S, T,
-		1.0, 1.0, 0.0,  1.0, 1.0, 1.0, 1.0,  T, T,
-		1.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,  T, S,
-		0.0, 0.0, 1.0,  1.0, 1.0, 1.0, 1.0,  S, T,
-		0.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0,  T, T,
-		1.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0,  T, S,
-		1.0, 0.0, 1.0,  1.0, 1.0, 1.0, 1.0,  S, S,
-	};
-	static GLuint itx[] = {0,1,2, 1,2,3, 4,5,7, 5,6,7, 0,4,5, 0,5,1, 2,6,7, 2,3,7, 6,1,5, 6,1,2, 0,3,4, 3,4,7};
 
 	void start_game() {
 		::init_engine(800, 600);
 
 		new Res::Texture((char*)"../Media/terrain.png", 0);
-		test_obj = GDrawable::create();
-		test_obj->vtx_c = sizeof(vtx) / sizeof(GLfloat);
-		test_obj->ind_c = sizeof(itx) / sizeof(GLuint);
-		test_obj->vertices = vtx;
-		test_obj->indices = itx;
-		test_obj->tri_mesh_count = sizeof(itx) / sizeof(GLuint) / 3;
-		GDrawable::fbind(test_obj);
+
+		for (int i = 0; i < 16; i++) {
+			t[i] = new Tile(i);
+		}
 
 		projection = glm::perspective(1.0f, float(VMDE->width) / float(VMDE->height), 1.0f, -1.0f);
 		view = glm::lookAt(glm::vec3(2.0, 3.0, 2.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
@@ -83,7 +105,10 @@ namespace VM76 {
 	}
 
 	void terminate() {
-		GDrawable::dispose(test_obj);
+		for (int i = 0; i < 16; i++) {
+			GDrawable::dispose(t[i]->obj);
+			free(t[i]);
+		}
 	}
 }
 
