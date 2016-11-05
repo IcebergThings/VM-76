b6e29f714bd25225806aec780c01b587dad33904
Frog Chen <satgo1546@yeah.net>
2016-11-05 15:36:38 +0800

    Windogix Make Update!
    本次更新新增了多项目轻松构建™支持。

--- make.rb
+++ make.rb
@@ -8,11 +8,23 @@
 #   ——Frog Chen《伪·浣溪沙·构建》
 #==============================================================================
 
+module Kernel
+	#--------------------------------------------------------------------------
+	# ● 历史遗留问题
+	#--------------------------------------------------------------------------
+	@@windows = (ENV["OS"] == "Windows_NT")
+	#--------------------------------------------------------------------------
+	# ● 请按任意键继续. . .
+	#--------------------------------------------------------------------------
+	def pause
+		system @@windows ? "pause" : "echo -n 'PRESS BUTTON'; read -n 1"
+	end
+end
+
 class WindogixMake
 	#--------------------------------------------------------------------------
 	# ● 常量与类变量
 	#--------------------------------------------------------------------------
-	@@windows = (ENV["OS"] == "Windows_NT")
 	SHORTCUT_NAME = (@@windows ? "make.bat" : "make")
 	#--------------------------------------------------------------------------
 	# ● 初始化对象
@@ -23,52 +35,44 @@ class WindogixMake
 	#--------------------------------------------------------------------------
 	# ● 输出帮助
 	#--------------------------------------------------------------------------
-	def display_help
-		puts <<~EOF
-			= Usage =
-			> ruby windoge_make.rb -D... -I... *.a *.lib -L... -l...
-			> rem 作为开发者，你不应该在库目录的名称中包含空格。
-			Typical arguments for Windoge:
-				-IC:\\Ruby23\\include\\ruby-2.3.0
-				-IC:\\Ruby23\\include\\ruby-2.3.0\\i386-mingw32
-				-DGLFW_DLL -DGLEW_STATIC
-				-ID:\\glfw-3.2.bin.WIN32\\include
-				-ID:\\glew-1.13.0\\include
-				-ID:\\glm-0.9.7.5
-				-ID:\\portaudio\\include
-				-ID:\\libogg-1.3.2\\include
-				-ID:\\libvorbis-1.3.5\\include
-				-ID:\\SOIL\\src
-				-O3 -Wl,--enable-auto-image-base,-subsystem,windows
-				D:\\SOIL\\lib\\libSOIL.a
-				-LD:\\bin -llibstdc++
-				-LD:\\glfw-3.2.bin.WIN32\\lib-mingw-w64
-				-LD:\\glew-1.13.0\\lib\\Release\\Win32
-				-lglfw3dll -lglew32s -lopengl32 -lportaudio_x86 -lvorbisfile
-			And for *nix:
-				-I~/SOIL/lib/include -I. -I..
-				$(pkg-config --cflags --libs glfw3 gl glm glew portaudio-2.0 ogg)
-				-lstdc++ -lSOIL
-			All ‘-Idir’ options will be translated to ‘-isystem dir’ and no way back.
-			libstdc艹 is for exceptions and other random stuff; omit it if not used.
-			-Wall, -Wextra and some necessary switches are built into this script.
-			‘D:\\bin’ above is where you put DLLs you've built yourself.
-			I recommend you to do ‘make.rb clean’ when you've changed build options.
-		EOF
-	end
-	#--------------------------------------------------------------------------
-	# ● 请按任意键继续. . .
-	#--------------------------------------------------------------------------
-	def pause
-		system @@windows ? "pause" : "echo -n 'PRESS BUTTON'; read -n 1"
+	def self.display_help(dir = nil)
+		if dir.nil?
+			puts <<~EOF
+				= Usage =
+				> ruby make.rb <directory> [clean]
+				> ruby make.rb --help [directory]
+				> ruby ..\\make.rb -D... -I... *.a *.lib -L... -l...
+				> ruby ..\\make.rb clean <garbage>
+				> rem 作为开发者，你不应该在库目录的名称中包含空格。
+				All ‘-Idir’ options will be translated to ‘-isystem dir’ and no way back.
+				-Wall, -Wextra and some necessary switches are built into this script.
+				I recommend you to do ‘make.rb clean’ when build options are changed.
+				No spaces or special characters in filenames. You know that.
+			EOF
+		else
+			puts "= #{dir} ="
+			puts File.read("#{dir}/flags.txt", :external_encoding => "utf-8")
+		end
 	end
 	#--------------------------------------------------------------------------
 	# ● 主程序
 	#--------------------------------------------------------------------------
 	def main
 		# display help when used incorrectly
-		if @argv.empty? or @argv.include?("--help") or @argv.include?("-h")
-			display_help
+		if @argv.empty? or ["--help", "-help", "-h"].include?(@argv.first)
+			WindogixMake.display_help(@argv[1])
+			return
+		end
+		# make <project>
+		if !FileTest.exist?("flags.txt")
+			Dir.chdir(@argv.first)
+			args = nil
+			File.open(SHORTCUT_NAME, "r") do |f|
+				f.readline
+				args = f.readline[4..-1].split(" ")
+			end
+			WindogixMake.new(args).main
+			Dir.chdir("..")
 			return
 		end
 		# make clean; make c l e a n
@@ -90,7 +94,7 @@ class WindogixMake
 				compiling_args << arg[2, arg.size]
 			else
 				puts "option not recognized: #{arg}"
-				exit
+				return
 			end
 		end
 		sources = Dir.glob("*.cpp")
@@ -143,26 +147,27 @@ class WindogixMake
 		end
 		File.open(SHORTCUT_NAME, "w") do |f|
 			f.write @@windows ? <<~BATCH : <<~SHELL
-				@ruby make.rb %1 ^
+				@echo off
+				rem #{@argv.join(" ")}
+				ruby ../make.rb %1 ^
 				#{@argv.join(" ^\n")}
 			BATCH
 				#!/bin/sh
-				ruby make.rb $1 \\
+				### #{@argv.join(" ")}
+				ruby ../make.rb $1 \\
 				#{@argv.join(" \\\n")}
 			SHELL
 			f.chmod(0755)
 		end
 	end
-#------------------------------------------------------------------------------
-# ◇ 为了避免不好的事发生而添加的类级错误处理
-#------------------------------------------------------------------------------
-rescue
-	p $!
-	pause
-	exit
 end
 
 #------------------------------------------------------------------------------
 # ◇ “各种定义结束后，从这里开始实际运行。”
 #------------------------------------------------------------------------------
-WindogixMake.new.main if __FILE__ == $0
+begin
+	WindogixMake.new.main if __FILE__ == $0
+rescue
+	p $!
+	pause
+end
