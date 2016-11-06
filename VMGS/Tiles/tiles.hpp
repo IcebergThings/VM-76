5c209bcff2b67f3cb7c884793e59f2955c17924b
Cheng Cao <bobcaocheng@163.com>
2016-11-06 19:41:14 +0800

    建立了“结构”的概念
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- VMGS/Tiles/tiles.hpp
+++ VMGS/Tiles/tiles.hpp
@@ -1,4 +1,4 @@
-#include "global.hpp"
+#include <global.hpp>
 
 #ifndef _INCLUDE_TILES_H
 #define _INCLUDE_TILES_H
@@ -7,15 +7,16 @@ namespace VM76 {
 	class Tile {
 	public:
 		GDrawable* obj[6] = {NULL, NULL, NULL, NULL, NULL, NULL};
+		glm::mat4 *mat = NULL;
 
 	private:
 		GLfloat *vtx[6] = {NULL, NULL, NULL, NULL, NULL, NULL};
 		GLuint *itx[6] = {NULL, NULL, NULL, NULL, NULL, NULL};
-		glm::mat4 *mat = NULL;
 
 	public:
 		Tile(int tid);
-		void render();
+		void render(bool r1, bool r2, bool r3, bool r4, bool r5, bool r6);
+		void update_instance(int mat_c, glm::mat4* mat);
 		void dispose();
 	};
 }
---
+++ VMGS/structure.cpp
@@ -0,0 +1,61 @@
+#include "global.hpp"
+
+namespace VM76 {
+
+	Block::Block(int id) {
+		tid = id;
+	}
+
+	void Structure::init_tiles() {
+		t[GRASS] = new Tile(0);
+		t[STONE] = new Tile(1);
+		t[DIRT] = new Tile(2);
+		t[WOOD] = new Tile(4);
+	}
+
+	void Structure::render() {
+		for (int x = 0; x < 16; x++) if (block_count[x] > 0)
+			t[x]->render(true, true, true, true, true, true);
+	}
+
+	void Structure::update() {
+		for (int x = 0; x < 16; x++) block_count[x] = 0;
+		for (int x = 0; x < 64; x++) {
+			for (int y = 0; y < 64; y++) {
+				for (int z = 0; z < 64; z++) if (str[x][y][z] != NULL) {
+					GLuint tid = str[x][y][z]->tid;
+					(mats[tid])[block_count[tid]] = glm::translate(
+						glm::mat4(1.0),
+						glm::vec3(float(x), float(y), float(z)) + wpos
+					);
+					block_count[tid] ++;
+				}
+			}
+		}
+
+		for (int x = 0; x < 16; x++) if (t[x])
+			t[x]->update_instance(block_count[x], &(mats[x])[0]);
+	}
+
+	void Structure::dispose() {
+		for (int x = 0; x < 16; x++) xefree(mats[x]);
+		for (int x = 0; x < 16; x++) if (t[x]) {
+			t[x]->dispose();
+			xefree(t[x]);
+		}
+	}
+
+	Structure::Structure() {
+		init_tiles();
+		memset(str, 0x00, sizeof(str));
+
+		for (int x = 0; x < 16; x++)
+			mats[x] = new glm::mat4[128];
+
+		block_count[0] = 1;
+		str[0][0][0] = new Block(0);
+
+		update();
+	}
+
+}
