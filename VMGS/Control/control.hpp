fad0700bae70b7e51a257559ba24f5404e4717b7
Cheng Cao <bobcaocheng@163.com>
2016-12-06 21:29:49 +0800

    Modulaize game control, now it is optional between game control mode (First person, god, etc.)
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

---
+++ VMGS/Control/control.hpp
@@ -0,0 +1,11 @@
+// Use this macro to decide control
+// #define Control_FPView
+// #define Control_GodView
+#define Control_DemoView
+
+namespace VM76 {
+
+	extern void init_control();
+	extern void update_control();
+
+}
--- VMGS/global.hpp
+++ VMGS/global.hpp
@@ -5,6 +5,7 @@
 #include <global.hpp>
 
 #include "Tiles/tile.hpp"
+#include "Control/control.hpp"
 
 #ifndef _INCLUDE_VMGS_GLOBAL_H
 	#define _INCLUDE_VMGS_GLOBAL_H
@@ -29,24 +30,6 @@
 		};
 
 		const GLfloat tile_size = 1.0f / 16.0f;
-
-		struct Game {
-			int
-				key_quit,
-				key_forward, key_left, key_right, key_back,
-				key_up, key_down;
-		};
-		extern Game game;
-
-		class Player {
-		public:
-			bool in_gui = false;
-			float horizontal_angle = 0.0;
-			float vertical_angle = 0.0;
-			float speed = 0.0;
-			glm::vec3 wpos = glm::vec3(0.0, 1.0, 0.0);
-		};
-		extern Player game_player;
 
 		class Block {
 		public:
