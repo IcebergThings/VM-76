18184eae9538dc613493b4fc0d143ebf2c753f5e
Cheng Cao <bobcaocheng@163.com>
2017-01-06 14:30:40 +0800

    加回混合
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- VMGS/main.cpp
+++ VMGS/main.cpp
@@ -16,11 +16,10 @@ namespace VM76 {
 			::main_draw_start();
 			update_control();
 
-			glClear(GL_COLOR_BUFFER_BIT);
-			glClear(GL_DEPTH_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-//			glEnable(GL_BLEND);
-//			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 			main_shader->use();
 
