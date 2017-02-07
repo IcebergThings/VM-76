7cd5bed9b5643d7f1d2dce92a966ee2e52e3f056
Cheng Cao <bobcaocheng@163.com>
2017-02-07 11:24:56 +0800

    渲染GUI，且可以按0-9切换预览的方块
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- VMGS/main.cpp
+++ VMGS/main.cpp
@@ -8,6 +8,7 @@
 
 namespace VM76 {
 	Shaders* shader_textured = NULL;
+	Shaders* gui = NULL;
 	Shaders* shader_basic = NULL;
 	Res::Texture* tile_texture = NULL;
 
@@ -15,11 +16,13 @@ namespace VM76 {
 	Cube* clist[16];
 	TiledMap* map;
 
+	glm::mat4 gui_2d_projection;
+
+	int hand_id = 1;
+
 	Axis* axe;
 
 	Object* obj = new Object();
-
-	int map_count = 0;
 
 	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 		#define PRESS(n) key == n && action == GLFW_PRESS
@@ -29,6 +32,19 @@ namespace VM76 {
 		if (PRESS(GLFW_KEY_S)) obj->move(glm::vec3(0.0, 0.0, 1.0));
 		if (PRESS(GLFW_KEY_UP)) obj->move(glm::vec3(0.0, 1.0, 0.0));
 		if (PRESS(GLFW_KEY_DOWN)) obj->move(glm::vec3(0.0, -1.0, 0.0));
+
+		if (PRESS(GLFW_KEY_0)) hand_id = 0;
+		if (PRESS(GLFW_KEY_0)) hand_id = 0;
+		if (PRESS(GLFW_KEY_1)) hand_id = 1;
+		if (PRESS(GLFW_KEY_2)) hand_id = 2;
+		if (PRESS(GLFW_KEY_3)) hand_id = 3;
+		if (PRESS(GLFW_KEY_4)) hand_id = 4;
+		if (PRESS(GLFW_KEY_5)) hand_id = 5;
+		if (PRESS(GLFW_KEY_6)) hand_id = 6;
+		if (PRESS(GLFW_KEY_7)) hand_id = 7;
+		if (PRESS(GLFW_KEY_8)) hand_id = 8;
+		if (PRESS(GLFW_KEY_9)) hand_id = 9;
+
 
 		if (PRESS(GLFW_KEY_SPACE)) {
 			Audio::play_sound("../Media/soft-ping.ogg", false);
@@ -52,6 +68,7 @@ namespace VM76 {
 
 			// Setup uniforms
 			shader_textured->set_float("brightness", VMDE->state.brightness);
+			shader_textured->set_texture("colortex0", tile_texture, 0);
 
 			// Textured blocks rendering
 			shader_textured->ProjectionView(projection, view);
@@ -68,23 +85,16 @@ namespace VM76 {
 			axe->render();
 
 			// GUI rendering
-			shader_textured->use();
+			gui->use();
+			gui->set_texture("atlastex", tile_texture, 0);
+			gui->ProjectionView(gui_2d_projection, glm::mat4(1.0));
 			glDisable(GL_DEPTH_TEST);
-			float aspectRatio = float(VMDE->width) / float(VMDE->height);
-			shader_textured->ProjectionView(
-				glm::ortho(0.0, 1.0 * aspectRatio, 0.0, 1.0, -1.0, 1.0),
-				glm::lookAt(glm::vec3(0.0,0.0,1.0), glm::vec3(0.0,0.0,0.0), glm::vec3(0.0,1.0,0.0))
-			);
-			int hand_id = 1;
-			clist[hand_id]->render();
+			if (hand_id > 0)
+				clist[hand_id - 1]->render();
 			glEnable(GL_DEPTH_TEST);
 
 			::main_draw_end();
 		} while (!VMDE->done);
-	}
-
-	void init_textures() {
-		shader_textured->set_texture("colortex0", tile_texture, 0);
 	}
 
 	void start_game() {
@@ -95,6 +105,9 @@ namespace VM76 {
 
 		shader_textured = Shaders::CreateFromFile("../Media/shaders/gbuffers_textured.vsh", "../Media/shaders/gbuffers_textured.fsh");
 		shader_basic = Shaders::CreateFromFile("../Media/shaders/gbuffers_basic.vsh", "../Media/shaders/gbuffers_basic.fsh");
+		gui = Shaders::CreateFromFile("../Media/shaders/gui.vsh", "../Media/shaders/gui.fsh");
+		float aspectRatio = float(VMDE->width) / float(VMDE->height);
+		gui_2d_projection = glm::ortho(0.0, 1.0 * aspectRatio, 0.0, 1.0, -1.0, 1.0);
 
 		projection = glm::perspective(1.3f, float(VMDE->width) / float(VMDE->height), 0.1f, 1000.0f);
 		view = glm::lookAt(glm::vec3(0.0, 2.6, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
@@ -111,11 +124,8 @@ namespace VM76 {
 		glClearDepth(1.0f);
 		glDepthMask(GL_TRUE);
 
-		init_textures();
-
 		block_pointer = new Cube(1);
 		// Set up hand block indicator's matrix
-		float aspectRatio = float(VMDE->width) / float(VMDE->height);
 		glm::mat4 block_display = glm::translate(glm::mat4(1.0), glm::vec3(0.02, 0.06, 0.2));
 		block_display = glm::scale(block_display, glm::vec3(0.1f));
 		block_display = glm::rotate(block_display, Util::PIf / 4.0f, glm::vec3(1.0, 0.0, 0.0));
