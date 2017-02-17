a1700ff3270c60252d7c99e10bdec9796aafcf03
Frog Chen <satgo1546@yeah.net>
2017-02-17 21:32:23 +0800

    砍掉V里的cpp文件
    我都佩服我自己，竟然能把一个好好的库弄成header-only的。
    
    可能是因为我不曾忘记受链接错误支配的恐惧。

--- V/VBinaryFileWriter.hpp
+++ V/VBinaryFileWriter.hpp
@@ -1,30 +1,39 @@
 //=============================================================================
-// ■ VBinaryFileWriter.cpp
+// ■ VBinaryFileWriter.hpp
 //-----------------------------------------------------------------------------
 //   在各计算机之间兼容的二进制文件写出类。（Little-endian）
 //=============================================================================
 
-#include "../global.hpp"
+#define ERROR_MESSAGE "error: lorem ipsum"
 
-namespace V {
-	#define ERROR_MESSAGE "error: lorem ipsum"
+class VBinaryFileWriter {
+public:
+	//-------------------------------------------------------------------------
+	// ● 成员
+	//-------------------------------------------------------------------------
+	FILE* f;
 	//-------------------------------------------------------------------------
 	// ● 构造
 	//-------------------------------------------------------------------------
-	VBinaryFileWriter::VBinaryFileWriter(const char* filename) {
+	VBinaryFileWriter(const char* filename) {
 		f = fopen(filename, "wb");
-		if (!f) error(ERROR_MESSAGE);
 	}
 	//-------------------------------------------------------------------------
 	// ● 析构
 	//-------------------------------------------------------------------------
-	VBinaryFileWriter::~VBinaryFileWriter() {
+	~VBinaryFileWriter() {
 		fclose(f);
 	}
 	//-------------------------------------------------------------------------
+	// ● 直接写入
+	//-------------------------------------------------------------------------
+		template <class T> void write_directly(T value) {
+			if (fwrite(&value, sizeof(T), 1, f) < 1) error("write_directly");
+		}
+	//-------------------------------------------------------------------------
 	// ● 写入1字节int
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_i8(int8_t value) {
+	void write_i8(int8_t value) {
 		write_u8((uint8_t) value);
 	}
 	//-------------------------------------------------------------------------
@@ -34,7 +43,7 @@ namespace V {
 		if (fputc(value & 0xff, f) < 0) error(ERROR_MESSAGE)
 	#define WRITE_CHAR_ \
 		if (fputc((value >>= 8) & 0xff, f) < 0) error(ERROR_MESSAGE)
-	void VBinaryFileWriter::write_i32(int32_t value) {
+	void write_i32(int32_t value) {
 		if (is_little_endian()) {
 			write_directly(value);
 		} else {
@@ -47,7 +56,7 @@ namespace V {
 	//-------------------------------------------------------------------------
 	// ● 写入8字节int
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_i64(int64_t value) {
+	void write_i64(int64_t value) {
 		if (is_little_endian()) {
 			write_directly(value);
 		} else {
@@ -66,31 +75,33 @@ namespace V {
 	//-------------------------------------------------------------------------
 	// ● 写入1字节unsigned
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_u8(uint8_t value) {
+	void write_u8(uint8_t value) {
 		if (fputc(value, f) < 0) error(ERROR_MESSAGE);
 	}
 	//-------------------------------------------------------------------------
 	// ● 写入4字节unsigned
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_u32(uint32_t value) {
+	void write_u32(uint32_t value) {
 		write_i32((int32_t) value);
 	}
 	//-------------------------------------------------------------------------
 	// ● 写入8字节unsigned
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_u64(uint64_t value) {
+	void write_u64(uint64_t value) {
 		write_i64((int64_t) value);
 	}
 	//-------------------------------------------------------------------------
 	// ● 写入4字节float
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_float(float value) {
+	void write_float(float value) {
 		write_directly<float>(value);
 	}
 	//-------------------------------------------------------------------------
 	// ● 写入8字节double
 	//-------------------------------------------------------------------------
-	void VBinaryFileWriter::write_double(double value) {
+	void write_double(double value) {
 		write_directly<double>(value);
 	}
-}
+};
+
+#undef ERROR_MESSAGE
--- V/VMath.hpp
+++ V/VMath.hpp
@@ -6,16 +6,42 @@
 
 namespace VMath {
 	//-------------------------------------------------------------------------
-	// ● 声明
+	// ● 常量
 	//-------------------------------------------------------------------------
-	extern const float PIf;
-	extern const double PI;
-	#define PId PI
-	extern const long double PIl;
-	void init();
-	void populate_sine_table();
+	// 先搞个模板变量
+	// 还不能用acos函数，会炸……（因为数学函数都不是constexpr）
+	// 这么长应该够了。但是不排除标准库里的PI是乱定义的，比如char PI = 3之类的。
+	template <class T> constexpr T pi =
+		(T) 3.141592653589793115997963468544185161590576171875;
+	// 然后弄出一堆π
+	const float PIf = pi<float>;
+	const double PI = pi<double>;
+	const double PId = PI;
+	const long double PIl = pi<long double>;
+	//-------------------------------------------------------------------------
+	// ● 正弦值表系统™
+	//   为了避免反复计算，将正弦值存储在这里。
+	//   当VMATH_SINE_TABLE_SIZE为256时，其分布为
+	//       [0] = sin 0
+	//       [64] = sin ⅛π
+	//       [128] = sin ¼π
+	//       [192] = sin ⅜π
+	//-------------------------------------------------------------------------
+	// 先搞个模板类
+	template <class T, size_t size> class SineTable {
+	public:
+		T value[size];
+		SineTable() {
+			T k = (T) .5 / (T) size * pi<T>;
+			for (size_t i = 0; i < size; i++) value[i] = sin((T) i * k);
+		}
+		T operator[](size_t i) const {
+			return value[i];
+		}
+	};
+	// 然后就实例化一个实例出来
 	#define VMATH_SINE_TABLE_SIZE ((size_t) 256)
-	extern float sine_table[VMATH_SINE_TABLE_SIZE];
+	const SineTable<float, VMATH_SINE_TABLE_SIZE> sine_table;
 	//-------------------------------------------------------------------------
 	// ● clamp
 	//-------------------------------------------------------------------------
