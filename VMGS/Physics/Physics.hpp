a89f3d57f886fefb16a2beb504fd9392276c10c4
Cheng Cao <bobcaocheng@163.com>
2017-08-04 10:46:52 +0800

    Initialize physics with this stuff
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

---
+++ VMGS/Physics/Physics.hpp
@@ -0,0 +1,39 @@
+//=============================================================================
+// ■ VMGS/Physics/Physics.hpp
+//-----------------------------------------------------------------------------
+// Take over the Physics world!
+//=============================================================================
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/fast_square_root.hpp>
+
+#ifndef _INCLUDE_PHYSICS_H
+#define _INCLUDE_PHYSICS_H
+
+typedef struct StructOBB {
+	glm::vec3 position;
+	glm::vec3 size;
+} OBB;
+
+class PhyEngine {
+public:
+	static bool AABB(OBB* a, OBB* b);
+
+	PhyEngine();
+};
+
+class BoxCollider {
+public:
+	glm::vec3 X, Y, Z;
+	glm::vec3 anchor;
+	glm::mat4 w2local;
+
+public:
+	bool is_collide(BoxCollider* b);
+
+	BoxCollider(glm::vec3 o, glm::vec3 a, glm::vec3 b, glm::vec3 c);
+};
+
+#endif
