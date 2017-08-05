d0b232ecb17363cb70d649c54b741ef49a2aaa7c
Frog Chen <satgo1546@yeah.net>
2017-08-05 14:56:07 +0800

    添加SuperXE

--- VMDE/global.hpp
+++ VMDE/global.hpp
@@ -91,6 +91,18 @@
 			(pointer) = NULL; \
 		} \
 	} while (false)
+	#define SuperXE(method, pointer) do { \
+		switch ((uintptr_t) pointer) { \
+		case 0xbaadf00d: \
+			(pointer) = NULL; \
+			[[fallthrough]]; \
+		case 0: \
+			break; \
+		default: \
+			method(pointer); \
+			(pointer) = NULL; \
+		} \
+	} while (false)
 	// VMDE_Dispose - 一键销毁宏魔法
 	#define VMDE_Dispose(method, object) do { \
 		if (object) { \
