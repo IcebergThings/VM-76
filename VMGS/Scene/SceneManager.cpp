02b097c16fad5999ee9e4e4a825f98f90b9b15b3
Frog Chen <satgo1546@yeah.net>
2017-08-22 21:10:59 +0800

    抽取SceneManager到单独的文件中

---
+++ VMGS/Scene/SceneManager.cpp
@@ -0,0 +1,58 @@
+//=============================================================================
+// ■ VMGS/Scene/SceneManager.cpp
+//=============================================================================
+
+#include "../VMGS.hpp"
+
+namespace VM76 { namespace SceneManager {
+	Scene* context = NULL;
+	bool render_debug_info = true;
+
+	void load_scene(Scene* c) {
+		context = c;
+	}
+
+	void update_scene() {
+		if (context) context->update();
+	}
+
+	void render_scene() {
+		if (context) context->render();
+		// Render FPS
+		if (render_debug_info) {
+			VMSC::disable_depth_test();
+			char fps[40];
+			sprintf(fps, "FPS: %d, %f.4ms", VMDE->fps, VMDE->frame_time);
+			TextRenderer::BakeOptions opt = {
+				.text = fps,
+				.width = 0.025,
+				.height = 0.05,
+				.decoration = TextRenderer::TextDecorationType::OUTLINE,
+				.color = glm::vec4(1.0, 1.0, 1.0, 1.0),
+			};
+			trex->instanceRenderText(
+				&opt,
+				gui_2d_projection,
+				glm::mat4(1.0),
+				glm::translate(glm::mat4(1.0), glm::vec3(0.01, 0.94, 0.0))
+			);
+			VMSC::enable_depth_test();
+		}
+	}
+
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+		if (key == GLFW_KEY_PAUSE && action == GLFW_PRESS && (mods & GLFW_MOD_CONTROL)) {
+			abort();
+		}
+		if (key == GLFW_KEY_F3 && action == GLFW_PRESS) {
+			render_debug_info = !render_debug_info;
+			return;
+		}
+		if (context) {
+			context->event_key(key, action);
+			if (action == GLFW_PRESS) {
+				context->event_keydown(key, mods);
+			}
+		}
+	}
+}}
