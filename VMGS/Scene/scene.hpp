7cbcef207b35469005e0c8ec1cc70a17b003e8a8
Frog Chen <satgo1546@yeah.net>
2017-08-23 18:08:49 +0800

    提取出scene.hpp
    说好的一个文件夹一个头文件。

---
+++ VMGS/Scene/scene.hpp
@@ -0,0 +1,81 @@
+//=============================================================================
+// ■ scene.hpp
+//=============================================================================
+
+#ifndef VMGS_SCENE_SCENE_HPP_
+#define VMGS_SCENE_SCENE_HPP_
+
+namespace VM76 {
+	//-------------------------------------------------------------------------
+	// ● Scenes
+	//-------------------------------------------------------------------------
+	class Scene : public Object {
+	public:
+		virtual void render() = 0; // Graphics update
+		virtual void update(); // Tick update
+		virtual void event_key(int key, int action) {};
+		virtual void event_keydown(int key, int mods) {};
+		virtual ~Scene();
+	};
+
+	class Scene_Editor : public Scene {
+	private:
+		Shaders shader_textured;
+		Shaders gui;
+		Shaders shader_basic;
+		Shaders final_composite;
+		Shaders deferred_lighting;
+		Shaders deferred_composite;
+		Res::Texture tile_texture = Res::Texture("../Media/terrain.png", NULL);
+
+		Cube block_pointer = Cube(1);
+		Tiles* clist[16];
+		Camera* cam;
+
+		int hand_id = 1;
+
+		Axis axe;
+
+		GObject* obj;
+
+		RenderBuffer* postBuffer;
+
+	public:
+		PhyEngine* physics;
+		PhysicsMap* phy_map;
+		Map map = Map(8, 8, 8, 32);
+
+	public:
+		Scene_Editor();
+		void event_key(int key, int action);
+		void event_keydown(int key, int mods);
+		void render();
+		void GenerateMap();
+		void update();
+		~Scene_Editor();
+	};
+
+	class Scene_Loading : public Scene {
+	private:
+		Scene* todo = NULL;
+
+	public:
+		Scene_Loading(Scene** tobeload);
+		void render();
+		void update();
+	};
+
+	//-------------------------------------------------------------------------
+	// ● Scene Management
+	//-------------------------------------------------------------------------
+	namespace SceneManager {
+		extern Scene* context;
+		extern bool render_debug_info;
+		void load_scene(Scene* c);
+		void render_scene();
+		void update_scene();
+		void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	};
+}
+
+#endif
--- VMGS/VMGS.hpp
+++ VMGS/VMGS.hpp
@@ -8,6 +8,7 @@
 #include "GameObject/GameObject.hpp"
 #include "Control/control.hpp"
 #include "Physics/Physics.hpp"
+#include "Scene/scene.hpp"
 
 #ifndef _INCLUDE_VMGS_GLOBAL_H
 namespace VM76 {
@@ -27,77 +28,6 @@ namespace VM76 {
 	extern TextRenderer* trex;
 	extern glm::mat4 gui_2d_projection;
 	extern float aspect_ratio;
-
-	//-------------------------------------------------------------------------
-	// ● Scenes
-	//-------------------------------------------------------------------------
-	class Scene : public Object {
-	public:
-		virtual void render() = 0; // Graphics update
-		virtual void update(); // Tick update
-		virtual void event_key(int key, int action) {};
-		virtual void event_keydown(int key, int mods) {};
-		virtual ~Scene();
-	};
-
-	class Scene_Editor : public Scene {
-	private:
-		Shaders shader_textured;
-		Shaders gui;
-		Shaders shader_basic;
-		Shaders final_composite;
-		Shaders deferred_lighting;
-		Shaders deferred_composite;
-		Res::Texture tile_texture = Res::Texture("../Media/terrain.png", NULL);
-
-		Cube block_pointer = Cube(1);
-		Tiles* clist[16];
-		Camera* cam;
-
-		int hand_id = 1;
-
-		Axis axe;
-
-		GObject* obj;
-
-		RenderBuffer* postBuffer;
-
-	public:
-		PhyEngine* physics;
-		PhysicsMap* phy_map;
-		Map map = Map(8, 8, 8, 32);
-
-	public:
-		Scene_Editor();
-		void event_key(int key, int action);
-		void event_keydown(int key, int mods);
-		void render();
-		void GenerateMap();
-		void update();
-		~Scene_Editor();
-	};
-
-	class Scene_Loading : public Scene {
-	private:
-		Scene* todo = NULL;
-
-	public:
-		Scene_Loading(Scene** tobeload);
-		void render();
-		void update();
-	};
-
-	//-------------------------------------------------------------------------
-	// ● Scene Management
-	//-------------------------------------------------------------------------
-	namespace SceneManager {
-		extern Scene* context;
-		extern bool render_debug_info;
-		void load_scene(Scene* c);
-		void render_scene();
-		void update_scene();
-		void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-	};
 
 	//-------------------------------------------------------------------------
 	// ● Game Logic
