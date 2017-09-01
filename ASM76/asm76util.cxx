e2c6f04ef82a1c54181d97cdf9f910e50a2358b9
Frog Chen <satgo1546@yeah.net>
2017-09-01 18:12:02 +0800

    Add `asm76util r`

--- ASM76/asm76util.cxx
+++ ASM76/asm76util.cxx
@@ -7,11 +7,14 @@
 #include "ASM76.hpp"
 
 void help(const char* argv0 = "asm76util") {
-	printf("Usage: %s <command> <argument 1> <argument 2>\n\n", argv0);
-	puts("c: assembly -> object code");
-	puts("d: assembly <- object code");
-	puts("e: print <argument 1> to stderr and exit(1)");
-	exit(0);
+	printf("Usage: %s <command> <argument 1> [argument 2]\n\n", argv0);
+	puts("c: assembly (1) -> object code (2)");
+	puts("d: assembly (2) <- object code (1)");
+	puts("e: print (1) to stderr and exit 1");
+	puts("r: run (1) with characters in (2) meaning the following:");
+	puts("    d: run a debug session");
+	puts("    r: dump registers in the end");
+	puts("    m: dump memory in the end");
 }
 
 void e(const char* msg) {
@@ -62,8 +65,39 @@ void d(const char* f1, const char* f2) {
 	free(p.instruct);
 }
 
+void r(const char* filename, const char* debug_flags) {
+	ASM76::Program p;
+	{
+		char* buf = read_file(filename);
+		if (memcmp(buf, ASM76::ObjectCode::magic_array, 8) == 0) {
+			p = ASM76::ObjectCode::read_file(filename);
+		} else {
+			ASM76::Assembler a(buf);
+			a.scan();
+			p = a.assemble();
+		}
+		delete[] buf;
+	}
+	bool d = false, r = false, m = false;
+	while (*debug_flags) switch (*debug_flags++) {
+	case 'd': d = true; break;
+	case 'r': r = true; break;
+	case 'm': m = true; break;
+	}
+	{
+		ASM76::VM v(p);
+		v.execute(d);
+		if (r) v.dump_registers();
+		if (m) v.dump_memory();
+	}
+	free(p.instruct);
+}
+
 int main(int argc, char** argv) {
-	if (argc != 4) help(argv[0]);
+	if (argc <= 1) goto z;
+	if (argv[1][1]) goto z;
+	if (strchr("cd", argv[1][0]) && argc != 4) goto z;
+	if (strchr("e", argv[1][0]) && argc != 3) goto z;
 	switch (argv[1][0]) {
 	case 'c':
 		c(argv[2], argv[3]);
@@ -74,6 +108,14 @@ int main(int argc, char** argv) {
 	case 'e':
 		e(argv[2]);
 		break;
+	case 'r':
+		r(argv[2], argc == 4 ? argv[3] : "");
+		break;
+	default:
+		goto z;
 	}
 	return 0;
+z:
+	help(argv[0]);
+	return 1;
 }
